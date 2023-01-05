385a42db49cadfc78b689860095ee411b5dba180
Brian-Miki <bmiki5508@gmail.com>
2023-01-04 23:24:16 -0800

    Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Typography, Grid, Card, CardMedia } from "@mui/material";
+import { Typography, Grid, Card, CardMedia, Button } from "@mui/material";
 import Profile from "./Profile.png";
 
 function App() {
   return (
-    <div>
-      <Grid container alignItems="center" direction="column">
-        <Typography variant="h1">Hi, I'm Brian</Typography>;
-        <Typography variant="h2">An Aspring Product Manager</Typography>;
-        <CardMedia>image = {Profile}</CardMedia>
+    <div style={{ textAlign: "center" }} className="text">
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        style={{ height: "100vh" }}
+      >
+        <Typography variant="h2">
+          <Grid item xs={12}>
+            <p>Hi, I'm Brian</p>
+          </Grid>
+          <Grid item xs={12}>
+            <p>An Aspiring Product Manager</p>
+          </Grid>
+        </Typography>
       </Grid>
     </div>
   );
